73bec8ab0fc5d93b55f5dc4ebc830a246c2b8269
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-05-16 15:52:52 +0200

    jwein3: Reducing parallel jobs from 4 to 2.
    Machine doesn't have enough RAM and cores for four concurrent jobs.

--- jwein3/local.cmake
+++ jwein3/local.cmake
@@ -4,5 +4,5 @@ set(USED_PROJECTS "VIENNAGRID" "VIENNAMESH")
 set(CTEST_BUILD_OPTIONS "-DBUILD_PYVIENNAMESH=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TOOLS=OFF")
 
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_BUILD_FLAGS -j4) # build tests in parallel
-set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4") # run tests in parallel
+set(CTEST_BUILD_FLAGS -j2) # build tests in parallel
+set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j2") # run tests in parallel
