a3fdd211c442e28d6778455589815dda464ba8f4
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-11-03 16:38:49 +0100

    Centos5: Turning off parallel builds (not enough RAM otherwise).

--- centos5/local.cmake
+++ centos5/local.cmake
@@ -4,5 +4,5 @@ set(USED_PROJECTS "VIENNAGRID" "VIENNAMESH")
 set(CTEST_BUILD_OPTIONS "-DBUILD_PYVIENNAMESH=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TOOLS=OFF -DVIENNAMESH_WITH_VGMODELER=OFF")
 
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_BUILD_FLAGS -j2) # build tests in parallel
+set(CTEST_BUILD_FLAGS) # build tests sequentially. Not enough RAM for parallel builds.
 set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j2") # run tests in parallel
